22548580c5a80ea1cd300676e39aa5c6384ab047
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2023-01-02 18:45:52 +0530

    Create 150. Evaluate Reverse Polish Notation.cpp

---
+++ Microsoft_company_1st/150. Evaluate Reverse Polish Notation.cpp
@@ -0,0 +1,43 @@
+
+// 150. Evaluate Reverse Polish Notation...
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> s;
+        for(string i:tokens){
+            if(i=="+"){
+                int a = s.top();
+                s.pop();
+                int b = s.top();
+                s.pop();
+                s.push(a+b);
+            }
+            else if(i=="-"){
+                int a = s.top();
+                s.pop();
+                int b = s.top();
+                s.pop();
+                s.push(b-a);
+            }
+            else if(i=="*"){
+                int a = s.top();
+                s.pop();
+                int b = s.top();
+                s.pop();
+                s.push(a*b);
+            }
+            else if(i=="/"){
+                int a = s.top();
+                s.pop();
+                int b = s.top();
+                s.pop();
+                s.push(b/a);
+            }
+            else{
+                s.push(stoi(i));
+            }
+        }
+        return s.top();
+    }
+};

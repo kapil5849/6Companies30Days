8cea41c3d7ef16cf08fd22584e0fcaef1ede2496
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2023-01-27 09:15:11 +0530

    Create 1461. Check If a String Contains All Binary Codes of Size K.cpp

---
+++ Flipkart_company_4th/1461. Check If a String Contains All Binary Codes of Size K.cpp
@@ -0,0 +1,16 @@
+
+
+//  1461. Check If a String Contains All Binary Codes of Size K...
+
+
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        if(s.size()<k){return false;}
+        set<string>s1;
+        for(int i=0; i<s.size()-k+1; i++){
+            s1.insert(s.substr(i, k));
+        }
+        return s1.size()==pow(2, k);
+    }
+};

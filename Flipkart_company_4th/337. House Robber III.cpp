79290476b0b995a00a705be15cac5be076eb8f5b
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2023-01-27 09:24:09 +0530

    Create 337. House Robber III.cpp

---
+++ Flipkart_company_4th/337. House Robber III.cpp
@@ -0,0 +1,23 @@
+
+
+// 337. House Robber III ...
+
+class Solution {
+public:
+    vector<int> traverser(TreeNode* curr){
+        if(!curr){
+            return {0, 0};
+        }
+        vector<int>l, r, t;
+        l = traverser(curr->left);
+        r = traverser(curr->right);
+        t.push_back( l[1] + r[1] + curr->val );
+        t.push_back( max(l[0], l[1]) + max(r[0], r[1]) ); 
+        return t;
+    }
+    int rob(TreeNode* root) {
+        vector<int>holder = traverser(root);
+        return max(holder[0], holder[1]);
+    }
+};
+
